3cafba1f8a941290c651fd4f7216be89d0e84d46
dchandlerp <chandler.prince@gmail.com>
2017-07-07 12:32:03 -0500

    I think this is my first commit after cloning. Completed all TODO items but still want to comment my codes and solidify my understanding.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -22,6 +22,22 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+
+        ArrayList<HashMap<String, String>> jobList;
+
+        if (searchType.equals("") || searchType.equals("all")){
+            jobList = JobData.findByValue(searchTerm);
+        }
+        else{
+            jobList = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("joblist", jobList);
+
+        return "search";
+    }
 
 }
